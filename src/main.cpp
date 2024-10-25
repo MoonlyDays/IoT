621de8204006fce8b6d525a255c777b592d44448
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-10-25 11:49:36 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include "stdio/serial.h"
 
 #include <Adafruit_ADXL345_U.h>
 
@@ -8,19 +9,20 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 void setup()
 {
     Serial.begin(9600);
+    serial_use_stdio();
+
     accel.begin();
     accel.setRange(ADXL345_RANGE_16_G);
 }
 
 void loop(void) 
 {
-  /* Get a new sensor event */ 
   sensors_event_t event; 
   accel.getEvent(&event);
  
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+  printf("X: %.2f ", event.acceleration.x);
+  printf("Y: %.2f ", event.acceleration.y);
+  printf("Z: %.2f ", event.acceleration.z);
+  
   delay(500);
 }
