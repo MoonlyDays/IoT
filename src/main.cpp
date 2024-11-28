b3bd9b30891b903e4ca02669e1e55519363c25c8
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-11-28 21:41:36 +0200

    lab3.2

--- src/main.cpp
+++ src/main.cpp
@@ -5,31 +5,38 @@
 #include "filters/sap.h"
 #include "filters/avg.h"
 
-#define PIN1 9
-#define PIN2 10
-#define TEMP 20.0
+#define LCD_COLS 20
+#define LCD_ROWS 4
+#define USS_PIN1 9
+#define USS_PIN2 10
+#define TEMPERATURE 20.0
 
-UltraSonicDistanceSensor m_DistanceSensor(PIN1, PIN2);
+LiquidCrystal_I2C m_LCD(0x27, LCD_COLS, LCD_ROWS);
+UltraSonicDistanceSensor m_DistanceSensor(USS_PIN1, USS_PIN2);
+
 SATFilter m_SATFilter;
 AVGFilter m_AVGFilter;
 
 void setup()
 {
-    Serial.begin(9600);
-    serial_use_stdio();
+    stdio_to_lcd(&m_LCD);
 }
 
 void loop(void) 
 {
-    double fDistance = m_DistanceSensor.measureDistanceCm(TEMP);
+    double fDistance = m_DistanceSensor.measureDistanceCm(TEMPERATURE);
     m_SATFilter.push(fDistance);
     m_AVGFilter.push(fDistance);
 
+    printf("| R: %.2f | ", fDistance);
+
     m_SATFilter.m_bFilled
-        ? printf("SAT> Filtered: %.2f", m_SATFilter.output())
-        : printf("SAT> Collecting data...");
+        ? printf("F1: %.2f | ", m_SATFilter.output())
+        : printf("F1:  ~~  | ");
 
     m_AVGFilter.m_bFilled
-        ? printf("AVG> Filtered: %.2f", m_AVGFilter.output())
-        : printf("AVG> Collecting data...");
+        ? printf("F2: %.2f | ", m_AVGFilter.output())
+        : printf("F2:  ~~  | ");
+
+    printf("\n");
 }
--- src/stdio/serial.h
+++ src/stdio/serial.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <LiquidCrystal_I2C.h>
 
 #ifndef SERIAL_H
 #define SERIAL_H
 
-void serial_use_stdio();
+void stdio_to_serial(unsigned long baud);
+void stdio_to_lcd(LiquidCrystal_I2C *lcd);
 
 #endif
