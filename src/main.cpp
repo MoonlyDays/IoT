17db4296372e79346c66c8661f2a0cbf2ed7ff73
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-11-28 23:13:24 +0200

    4.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,49 @@
 #include <Arduino.h>
+#include <L298N.h>
 
+#include "stdio/serial.h"
+#include "util/prompts.h"
 #include "drivers/relay.h"
-#include "drivers/motor.h"
 #include "drivers/potentiometer.h"
 
-#define MOTOR_PINA A2
-#define MOTOR_PINB A3
-#define PMETER_PIN A4
+#define LCD_COLS 20
+#define LCD_ROWS 4
+#define MOTOR_PIN1 7
+#define MOTOR_PIN2 8
 
-Potentiometer m_Potentiometer(PMETER_PIN);
-Motor m_Motor(MOTOR_PINA, MOTOR_PINB);
+LiquidCrystal_I2C m_LCD(0x27, LCD_COLS, LCD_ROWS);
+L298N m_Motor(MOTOR_PIN1, MOTOR_PIN2);
 
 void setup()
 {
-    Serial.begin(9600);
+    stdio_to_serial(9600);
+    stdio_to_lcd(&m_LCD);
+
+    m_LCD.init();
+    m_LCD.backlight();
 }
 
 void loop()
 {
-    int value = m_Potentiometer.read();
-    int power = map(value, 0, 1023, -100, 100);
-    m_Motor.setSpeed(power);
+    char buf[32];
+    prompt(NULL, buf, sizeof(buf));
+    printf("Command: %s\n", buf);
+
+    char *token = strtok(buf, " ");
+    if (token && strcmp(token, "SPEED") == 0)
+    {
+        token = strtok(NULL, buf);
+        int speed = atoi(token);
+
+        if(speed < -100)  speed = -100;
+        if(speed > 100)   speed = 100;
+
+        speed = map(speed, -100, 100, 0, 0xFF);
+        m_Motor.setSpeed(speed);
+
+        printf("Changing MOTOR speed to %d\n", speed);
+        return;
+    }
+
+    printf("Invalid command \"%s\".\n", buf); 
 }
