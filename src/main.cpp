fe8d4740802386886ffcdb125f2467967f8651e9
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-12-04 19:40:24 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,99 +1,101 @@
 #include <Arduino.h>
 
-#include "drivers/button.h"
-#include "stdio/serial.h"
+#include "drivers/led.h"
 
-#define BUTTON_PIN 2
+#define RED_LED_PIN 2
+#define YELLOW_LED_PIN 3
+#define GREEN_LED_PIN 4
 
-enum State
+LED m_Red(RED_LED_PIN);
+LED m_Yellow(YELLOW_LED_PIN);
+LED m_Green(GREEN_LED_PIN);
+
+enum TrafficLightState
 {
-    STATE_IDLE,
-    STATE_SHORT_PRESS,
-    STATE_LONG_PRESS,
-    STATE_RELEASED,
-    STATE_DOUBLE_PRESS
+    STATE_RED,
+    STATE_RED_YELLOW,
+    STATE_GREEN,
+    STATE_YELLOW,
 };
 
-Button m_Button(BUTTON_PIN);
+TrafficLightState currentState = STATE_RED;
 
-State currentState = STATE_IDLE;
-unsigned long lastPressTime = 0;
-unsigned long buttonHoldTime = 0;
-bool lastButtonState = LOW;
-unsigned long doublePressTimeout = 300;
-unsigned long longPressThreshold = 1000;
-unsigned long lastReleaseTime = 0;
+unsigned long lastStateChangeTime = 0;
+
+const unsigned long RED_DURATION = 5000;
+const unsigned long RED_YELLOW_DURATION = 2000;
+const unsigned long GREEN_DURATION = 5000;
+const unsigned long YELLOW_DURATION = 2000;
+
+void setTrafficLight(TrafficLightState state)
+{
+    m_Red.turnOff();
+    m_Yellow.turnOff();
+    m_Green.turnOff();
+
+    switch (state)
+    {
+    case STATE_RED:
+        m_Red.turnOn();
+        break;
+    case STATE_RED_YELLOW:
+        m_Yellow.turnOn();
+        break;
+    case STATE_GREEN:
+        m_Green.turnOn();
+        break;
+    case STATE_YELLOW:
+    m_Yellow.turnOn();
+        break;
+    }
+}
 
 void setup()
 {
-    stdio_to_serial(9600);
+    setTrafficLight(currentState);
+    lastStateChangeTime = millis();
 }
 
 void loop()
 {
-    bool buttonState = m_Button.down();
+    unsigned long currentTime = millis();
 
     switch (currentState)
     {
-    case STATE_IDLE:
-        if (buttonState)
+    case STATE_RED:
+        if (currentTime - lastStateChangeTime >= RED_DURATION)
         {
-            currentState = STATE_SHORT_PRESS;
-            lastPressTime = millis();
-            printf("STATE_SHORT_PRESS\n");
+            currentState = STATE_RED_YELLOW;
+            lastStateChangeTime = currentTime;
+            setTrafficLight(currentState);
         }
         break;
 
-    case STATE_SHORT_PRESS:
-        if (buttonState)
+    case STATE_RED_YELLOW:
+        if (currentTime - lastStateChangeTime >= RED_YELLOW_DURATION)
         {
-            buttonHoldTime = millis() - lastPressTime;
-            if (buttonHoldTime > longPressThreshold)
-            {
-                currentState = STATE_LONG_PRESS;
-                printf("STATE_LONG_PRESS\n");
-            }
-        }
-        else
-        {
-            lastReleaseTime = millis();
-            if (millis() - lastPressTime < longPressThreshold)
-            {
-                currentState = STATE_RELEASED;
-                printf("STATE_RELEASED\n");
-            }
+            currentState = STATE_GREEN;
+            lastStateChangeTime = currentTime;
+            setTrafficLight(currentState);
         }
         break;
 
-    case STATE_LONG_PRESS:
-        if (!buttonState)
+    case STATE_GREEN:
+        if (currentTime - lastStateChangeTime >= GREEN_DURATION)
         {
-            currentState = STATE_IDLE;
-            printf("STATE_IDLE\n");
+            currentState = STATE_YELLOW;
+            lastStateChangeTime = currentTime;
+            setTrafficLight(currentState);
         }
         break;
 
-    case STATE_RELEASED:
-        if (buttonState && (millis() - lastReleaseTime < doublePressTimeout))
+    case STATE_YELLOW:
+        if (currentTime - lastStateChangeTime >= YELLOW_DURATION)
         {
-            currentState = STATE_DOUBLE_PRESS;
-            printf("STATE_DOUBLE_PRESS\n");
-        }
-        else if (millis() - lastReleaseTime >= doublePressTimeout)
-        {
-            currentState = STATE_IDLE;
-            printf("STATE_IDLE\n");
-        }
-        break;
-
-    case STATE_DOUBLE_PRESS:
-        if (!buttonState)
-        {
-            currentState = STATE_IDLE;
-            printf("STATE_IDLE\n");
+            currentState = STATE_RED;
+            lastStateChangeTime = currentTime;
+            setTrafficLight(currentState);
         }
         break;
     }
-
-    delay(10);
 }
