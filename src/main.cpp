f502c001cc17e81f5ea31fc00bae1ffcc01cb6c7
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-12-04 18:39:57 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,67 @@
+#include <PID_v1_bc.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
-#include <DHT.h>
 
+#include "drivers/motor.h"
 #include "stdio/serial.h"
-#include "drivers/potentiometer.h"
-#include "drivers/relay.h"
+#include "util/prompts.h"
 
-#define POTENTIOMETER_PIN A1
-#define RELAY_PIN 3
-#define DHT_PIN 2
-#define DHT_TYPE DHT11
-#define HYSTERESIS 1.0
+#define ENA 8
+#define IN1 7
+#define IN2 6
 
-Relay m_Relay(RELAY_PIN);
-DHT m_DHT(DHT_PIN, DHT_TYPE);
-Potentiometer m_Potentiometer(POTENTIOMETER_PIN);
-LiquidCrystal_I2C m_LCD(0x20, 20, 4);
+Motor m_Motor(ENA, IN1, IN2);
+LiquidCrystal_I2C m_LCD(0x20, 16, 2);
 
-float setPoint = 25.0;
-float currentTemperature = 0;
-bool relayState = false;
+double m_iSetPoint, m_iInput, m_iOutput;
+double Kp = 2.0, Ki = 5.0, Kd = 1.0;
+
+PID myPID(&m_iInput, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
     stdio_to_serial(9600);
     stdio_to_lcd(&m_LCD);
-    m_DHT.begin();
+
+    myPID.SetMode(AUTOMATIC);
+    myPID.SetOutputLimits(0, 255);
 
     m_LCD.init();
     m_LCD.backlight();
-    m_LCD.setCursor(0, 0);
-
-    m_Relay.turnOff();
 }
 
 void loop()
 {
-    currentTemperature = m_DHT.readTemperature();
+    char buf[32];
+    prompt(NULL, buf, sizeof(buf));
+    int newSetpoint = atoi(buf);
 
-    int potValue = m_Potentiometer.read();
-    potValue = map(potValue, 0, 1024, 10, 35);
+    if (newSetpoint >= 0 && newSetpoint <= 255)
+    {
+        m_iSetPoint = newSetpoint;
+        printf("Setpoint: %d\n", m_iSetPoint);
+    }
+    else
+    {
+        printf("Please enter a value between 0 and 255.\n");
+    }
 
-    if (currentTemperature < setPoint - HYSTERESIS)
-        relayState = true;
-    else if (currentTemperature > setPoint + HYSTERESIS)
-        relayState = false;
+    if (m_iInput < m_iSetPoint)
+    {
+        m_iInput += m_iOutput * 0.1;
+        if (m_iInput > m_iSetPoint)
+            m_iInput = m_iSetPoint;
+    }
+    else if (m_iInput > m_iSetPoint)
+    {
+        m_iInput -= m_iOutput * 0.1;
+        if (m_iInput < m_iSetPoint)
+            m_iInput = m_iSetPoint;
+    }
 
-    m_Relay.setState(relayState);
-    printf(
-        "Temp: %d | SetPoint: %d | Relay: %s",
-        currentTemperature, potValue, relayState ? "ON" : "OFF");
+    m_iOutput = myPID.Compute();
+    m_Motor.setSpeed(m_iOutput);
 
-    delay(2000);
+    printf("SetPoint: %d | Current %d", m_iSetPoint, m_iInput);
+    delay(100);
 }
