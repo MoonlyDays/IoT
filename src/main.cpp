3414588aa4030c26b7db79fc209696f1d2e54af1
Daniel Basiuc-Brinzei <moonlydays1@gmail.com>
2024-12-04 18:26:36 +0200

    5.1

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,54 @@
-#include <Arduino.h>
 #include <LiquidCrystal_I2C.h>
+#include <DHT.h>
 
 #include "stdio/serial.h"
-#include "util/prompts.h"
-#include "drivers/motor.h"
+#include "drivers/potentiometer.h"
+#include "drivers/relay.h"
 
-#define ENA 8
-#define IN1 7
-#define IN2 6
+#define POTENTIOMETER_PIN A1
+#define RELAY_PIN 3
+#define DHT_PIN 2
+#define DHT_TYPE DHT11
+#define HYSTERESIS 1.0
 
-LiquidCrystal_I2C m_LCD(0x27, 20, 4);
-Motor m_Motor(ENA, IN1, IN2);
+Relay m_Relay(RELAY_PIN);
+DHT m_DHT(DHT_PIN, DHT_TYPE);
+Potentiometer m_Potentiometer(POTENTIOMETER_PIN);
+LiquidCrystal_I2C m_LCD(0x20, 20, 4);
+
+float setPoint = 25.0;
+float currentTemperature = 0;
+bool relayState = false;
 
 void setup()
 {
     stdio_to_serial(9600);
     stdio_to_lcd(&m_LCD);
+    m_DHT.begin();
 
     m_LCD.init();
     m_LCD.backlight();
+    m_LCD.setCursor(0, 0);
+
+    m_Relay.turnOff();
 }
 
 void loop()
 {
-    char buf[32];
-    prompt(NULL, buf, sizeof(buf));
-    printf("Command: %s\n", buf);
+    currentTemperature = m_DHT.readTemperature();
 
-    char *token = strtok(buf, " ");
-    if (token && strcmp(token, "SPEED") == 0)
-    {
-        token = strtok(NULL, buf);
-        int speed = atoi(token);
+    int potValue = m_Potentiometer.read();
+    potValue = map(potValue, 0, 1024, 10, 35);
 
-        if(speed < -100)  speed = -100;
-        if(speed > 100)   speed = 100;
+    if (currentTemperature < setPoint - HYSTERESIS)
+        relayState = true;
+    else if (currentTemperature > setPoint + HYSTERESIS)
+        relayState = false;
 
-        m_Motor.setSpeed(speed);
+    m_Relay.setState(relayState);
+    printf(
+        "Temp: %d | SetPoint: %d | Relay: %s",
+        currentTemperature, potValue, relayState ? "ON" : "OFF");
 
-        printf("Changing MOTOR speed to %d\n", speed);
-        return;
-    }
-
-    printf("Invalid command \"%s\".\n", buf); 
+    delay(2000);
 }
